f9b27178f3cad994ab4627b735e508bf199f5b7a
Marek Połom <marekpolom22@gmail.com>
2020-11-10 18:43:36 +0100

    zadanie 2

--- zadanie2.py
+++ zadanie2.py
@@ -16,12 +16,23 @@ class Password:
         True
         >>> c.ValidPassword('H@s10')
         False
+        >>> c.ValidPassword(c)
+        Traceback (most recent call last):
+           File "C:\\Users\\Marek\\AppData\\Local\\Programs\\Python\\Python39\\lib\\doctest.py", line 1336, in __run
+              exec(compile(example.source, filename, "single",
+           File "<doctest __main__.Password.ValidPassword[7]>", line 1, in <module>
+              c.ValidPassword(c)
+           File "F:\\Marek\\ug\\testowanie-automatyczne\\laboratorium-6-marekpolom\\zadanie2.py", line 28, in ValidPassword
+              raise TypeError("Invalid type: {}".format(type(pwd)))
+        TypeError: Invalid type: <class '__main__.Password'>
         """
-
-        if re.fullmatch(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,}$', str(pwd)):
-            return True
+        if (type(pwd) == str or type(pwd) == int):
+            if re.fullmatch(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,}$', str(pwd)):
+                return True
+            else:
+                return False
         else:
-            return False
+            raise TypeError("Invalid type: {}".format(type(pwd)))
 
 if __name__ == "__main__":
     import doctest
